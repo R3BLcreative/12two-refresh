f404d0dc5d338b2b089a16687b783162042ba479
James Cook <ha-jcook@James-Cook.local>
2023-01-11 14:26:43 -0600

    Tweaked responsive styles for main-nav-items

--- resources/views/components/main-nav-item.blade.php
+++ resources/views/components/main-nav-item.blade.php
@@ -8,8 +8,8 @@
 
 @php
 $classes = [
-'default' => 'text-body-800 font-serif font-semibold mobile:text-xl tablet:text-2xl laptop:text-xl uppercase tracking-widest transition-all ease-in-out hover:text-primary-300 active:text-primary-700',
-'active' => 'text-primary-500 font-serif font-semibold mobile:text-xl tablet:text-2xl laptop:text-xl uppercase tracking-widest transition-all ease-in-out hover:text-primary-500 active:text-primary-700',
+'default' => 'text-body-800 font-serif font-semibold mobile:text-xl mobile:pl-5 mobile:border-l-8 mobile:border-l-primary-500 tablet:text-2xl laptop:pl-0 laptop:border-0 laptop:text-xl uppercase tracking-widest transition-all ease-in-out hover:text-primary-300 active:text-primary-700',
+'active' => 'text-primary-500 font-serif font-semibold mobile:text-xl mobile:pl-5 mobile:border-l-8 mobile:border-l-primary-500 tablet:text-2xl laptop:pl-0 laptop:border-0 laptop:text-xl uppercase tracking-widest transition-all ease-in-out hover:text-primary-500 active:text-primary-700',
 ];
 
 if(request()->segment(1) == $route) {
@@ -20,7 +20,7 @@ $class = $classes['default'] . ' ' . $class;
 @endphp
 
 <li class="relative group">
-	<a href="{{ ($route) ? route($route) : $href }}" class="py-4 {{ $class }}">
+	<a href="{{ ($route) ? route($route) : $href }}" class="laptop:py-4 {{ $class }}">
 		{{ $text }}
 	</a>
 
