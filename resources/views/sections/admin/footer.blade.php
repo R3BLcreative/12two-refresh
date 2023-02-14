0a4195fba201651122a81a1d38dd2de7c32188e8
James Cook <james.cook@helloalice.com>
2023-02-14 16:41:04 -0600

    Updated admin footer to include CMS name and trademark symbol

--- resources/views/sections/admin/footer.blade.php
+++ resources/views/sections/admin/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="flex items-center justify-start w-full min-h-[55px] px-4 z-49 border-t text-xs bg-white" role="contentinfo">
-	R3BL Creative LLC. &copy; {{ date('Y') }}
+	Infinite CMS &trade; &nbsp;|&nbsp; R3BL Creative LLC. &copy; {{ date('Y') }}
 </footer>
